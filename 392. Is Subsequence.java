c55fb94c1cee3351f0a8e71ac41b0fab6cbe5194
Hanzhi Yang <383916752@qq.com>
2018-05-12 23:43:34 +0800

    Update 392. Is Subsequence.java

--- 392. Is Subsequence.java
+++ 392. Is Subsequence.java
@@ -11,3 +11,14 @@ class Solution {
         return s_idx == s.length();
     }
 }
+
+// a more fast solution with the helper of API
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        for ( int i = 0, pos = 0; i < s.length(); i++, pos++ ){
+            pos = t.indexOf(s.charAt(i), pos);
+            if ( pos == -1 ) return false;
+        }
+        return true;
+    }
+}
