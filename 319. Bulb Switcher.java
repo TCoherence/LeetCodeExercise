e142d168c846d8bcd8997b515ad37041ef3b9f70
Hanzhi Yang <383916752@qq.com>
2018-05-24 23:44:40 +0800

    Create 319. Bulb Switcher.java

---
+++ 319. Bulb Switcher.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
+
+// Easy math solution:
+// the factor of square number will be a odd count, which means it will be on at last.
