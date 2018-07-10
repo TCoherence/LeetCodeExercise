402eace78e23bfb84240b47ffd11354256ebbef6
Hanzhi Yang <383916752@qq.com>
2018-07-10 20:25:48 +0800

    Update a new efficient solution.

--- 212. Word Search II.java
+++ 212. Word Search II.java
@@ -44,3 +44,72 @@ class Solution {
 }
 
 // need to improve
+        /* Three points to be noticed:
+         * 1. how to avoid repeating searching paths.
+         * 2. how to avoid repeating elements(words)
+         * 3. in backtracking, when to stop.
+         */
+        // check para
+        if ( board == null || board.length == 0 || board[0].length == 0 || words == null || words.length == 0 ) return new ArrayList<String>();
+        HashSet<String> set = new HashSet<>();
+        List<String> res = new ArrayList<>();
+        TrieNode root = new TrieNode();
+        // build Trie-Tree
+        for ( String word : words ){
+            addWord ( word, root );
+        }
+        // search in board
+        for ( int i = 0; i < board.length; i++ ) {
+            for ( int j = 0; j < board[0].length; j++ ) {
+                boolean tmp = isExist(board, i, j, root, res, set);
+            }
+        }
+        return res;
+        
+    }
+    
+    public boolean isExist(char[][] board, int i, int j, TrieNode curNode, List<String> res, Set<String> set) {
+        // when to stop
+        if ( !curNode.word.equals("") ) {
+            if ( !set.contains(curNode.word) ) {
+                res.add(curNode.word);
+                set.add(curNode.word);
+            } 
+        }
+        // isValidIndex
+        if ( i < 0 || i >= board.length || j < 0 || j >= board[0].length ) return false;
+        // isValidValue
+        if ( board[i][j] - 'a' < 0 || board[i][j] - 'a' > 25 ) return false;
+        
+        char curChar = board[i][j];
+        board[i][j] -= 26;
+        boolean ret = false;
+        if ( curNode.child[curChar - 'a'] != null ) {
+            ret =   isExist(board, i + 1, j, curNode.child[curChar - 'a'], res, set) ||
+                    isExist(board, i - 1, j, curNode.child[curChar - 'a'], res, set) ||
+                    isExist(board, i, j + 1, curNode.child[curChar - 'a'], res, set) ||
+                    isExist(board, i, j - 1, curNode.child[curChar - 'a'], res, set);
+        }
+        board[i][j] += 26;
+        return ret;
+        
+        
+    }
+    
+    private class TrieNode {
+        public String       word    = "";
+        public TrieNode[]   child   = new TrieNode[26];
+    }
+    
+    private void addWord(String word, TrieNode root){
+        if ( word == null || word.equals(null) ) return ;
+        
+        for ( char c : word.toCharArray() ){
+            int index = c - 'a';
+            if ( root.child[index] == null ){
+                root.child[index] = new TrieNode();
+            }
+            root = root.child[index];
+        }
+        root.word = word;
+    }
