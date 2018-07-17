49fa710f354c6dfff883ebc7a7f4a5444813b992
Hanzhi Yang <383916752@qq.com>
2018-07-17 23:55:58 +0800

    Create 219. Contains Duplicate II.java

---
+++ 219. Contains Duplicate II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // check param
+        if ( k <= 0 || nums == null || nums.length == 0 ) return false;
+        
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for ( int i = 0; i < nums.length; i++ ) {
+            int num = nums[i];
+            if ( map.containsKey(num) && (i - map.get(num)) <= k) 
+                return true;
+            map.put(num, i);
+        }
+        
+        return false;
+    }
+}
