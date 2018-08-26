4ddc5fb35db3092ec4776f6f7ed5b686b2623706
Hanzhi Yang <383916752@qq.com>
2018-08-26 19:27:04 +0800

    Update 22. Generate Parentheses.java
    update memory usage optimization.

--- 22. Generate Parentheses.java
+++ 22. Generate Parentheses.java
@@ -33,3 +33,41 @@ class Solution {
         }
     }
 }
+
+// REDO
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ret = new ArrayList<String>();
+        if ( n == 0 ) {
+            ret.add("");
+            return ret;
+        }
+        if ( n < 0 ) {
+            return ret;
+        } 
+        StringBuilder sb = new StringBuilder();
+        generateHelper(sb, n, n, ret);
+        return ret;
+    }
+    public void generateHelper(StringBuilder sb, int left, int right, List<String> res){
+        
+
+        // come in first from "("
+        if ( left > 0 ){
+            // StringBuilder sbnow = sb;// new StringBuilder(sb);
+            sb.append("(");
+            generateHelper(sb, left - 1, right, res);
+        }
+        // if there are more ')' out there, put in first to ensure left >= right
+        if ( left < right){
+            // StringBuilder sbnow = sb; //new StringBuilder(sb);
+            sb.append(")");
+            generateHelper(sb, left, right - 1, res);
+        }
+        if ( left == 0 && right == 0 ) {
+            res.add(sb.toString());
+            // return;
+        }
+        if ( sb.length() > 0 ) sb.deleteCharAt(sb.length() - 1);
+    }
+}
