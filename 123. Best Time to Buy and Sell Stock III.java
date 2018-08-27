155be098eb04cc3ddc69726f2d54dbaa94163b27
Hanzhi Yang <383916752@qq.com>
2018-08-27 16:12:27 +0800

    Update 123. Best Time to Buy and Sell Stock III.java
    Redo

--- 123. Best Time to Buy and Sell Stock III.java
+++ 123. Best Time to Buy and Sell Stock III.java
@@ -30,3 +30,45 @@ class Solution {
         return s0[k];
     }
 }
+
+// redo
+class Solution {
+    public int maxProfit(int[] prices) {
+        return maxProfithelper(2, prices);
+    }
+    private int maxProfithelper(int k, int[] prices) {
+        if ( prices == null ) return 0;
+        if ( prices.length == 0 ) return 0;
+        // case optimization
+        if ( k >= (prices.length >>> 1) ) {
+            // prices.length / 2 <= k, become problem that we can buy and sold in scale of groups.
+            int sum = 0, min = prices[0], max = prices[0];
+            for ( int i = 0; i < prices.length; i++ ) {
+                if ( prices[i] > max )  {
+                    max = prices[i];
+                    if ( i == prices.length - 1 ) {
+                        sum += max - min;
+                        min = prices[i];
+                    }
+                }
+                else {
+                    sum += max - min;
+                    min = prices[i];
+                    max = prices[i];
+                }
+            }
+            return sum;
+            
+        }
+        int[] soldAt = new int[k + 1];
+        int[] buyAt = new int[k + 1];
+        Arrays.fill(buyAt, Integer.MIN_VALUE);
+        for ( int price : prices ) {
+            for ( int i = 1; i <= k; i++ ) {
+                soldAt[i] = Math.max(soldAt[i], buyAt[i] + price);
+                buyAt[i] = Math.max(buyAt[i], soldAt[i - 1] - price);
+            }
+        }
+        return soldAt[k];
+    }
+}
