170f8e5869fa40594cf2e5b58b605b7d44b044df
Hanzhi Yang <383916752@qq.com>
2018-08-27 16:28:17 +0800

    Update 42. Trapping Rain Water.java

--- 42. Trapping Rain Water.java
+++ 42. Trapping Rain Water.java
@@ -38,3 +38,34 @@ class Solution {
 }
 
 // Two pointers, we can go through from two sides or from the same side!!! It is very important to keep this in mind.
+
+
+// redo , more concise
+class Solution {
+    public int trap(int[] height) {
+        if ( height == null || height.length == 0 || height.length == 1 ) return 0;
+        int left = 0, right = height.length - 1;
+        while ( height[left]  == 0 ) left++;
+        while ( height[right] == 0 ) right--;
+        int sum = 0;
+        while( left < right ) {
+            if ( height[left] < height[right] ) {
+                int l = left + 1;
+                while ( l < right && height[l] < height[left] ) {
+                    sum += height[left] - height[l];
+                    l++;
+                }
+                left = l;
+            }
+            else {
+                int r = right - 1;
+                while ( r > left && height[r] < height[right] ) {
+                    sum += height[right] - height[r];
+                    r--;
+                }
+                right = r;
+            }
+        }
+        return sum;
+    }
+}
